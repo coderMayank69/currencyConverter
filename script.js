c38b530819437c561a010d88eb13dee853eee0ec
coderMayank.69 <mayankbca96325@gmail.com>
2024-12-22 18:21:36 +0530

    updated

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
 
 const dropdown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector(" form button");
 
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
 for (let select of dropdown) {
-    for (currCode in countryList) {
+    for (currCode in (countryList)) {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
@@ -23,5 +30,29 @@ for (let select of dropdown) {
 }
 
 const updateFlag = (element) => {
-    console.log(element);
-}
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    element.parentElement.querySelector("img").src = newSrc;
+};
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateExchangeRate();
+
+});
+
+const updateExchangeRate = async() => {
+    let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if (amtValue === "" || amtValue < 1) {
+        amtValue = 1;
+        amount.value = "1";
+    }
+    const URL = `${BASE_URL}${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let Rate = data[toCurrency.value.toLowerCase()];
+    let finalAmt = amount * Rate;
+    msg.innerText = `${amtValue} ${fromCurrency.value} = ${finalAmt} ${toCurrency.value}`;
+};
